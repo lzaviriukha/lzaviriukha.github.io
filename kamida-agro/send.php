18453585f2fc9bfa5c726f8b7d8a6154e9fb1993
lzaviriukha <leozaviriukha@ukr.net>
2017-09-13 20:44:37 +0300

    layout
    new

--- kamida-agro/send.php
+++ kamida-agro/send.php
@@ -1,49 +1,17 @@
-<?
-// ------------- конфигурация ---------------//
-
-$adminemail="leozaviriukha@ukr.net"; // e-mail админа
-
-$date=date("d.m.y"); // число.месяц.го
-$time=date("H:i"); // часы:минуты:секунды
-$backurl="https://lzaviriukha.github.io/kamida-agro/"; // на какую старинцу переходит после отправки
-
-//--------------------------------------------//
-
-// прринимаем данные с формы
-
-$email=$_POST['email'];
-
-//проверяем валидность e-mail
-
-if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", strtolower($email)))
-
-  { 
-
- 	echo 
-  " <center>Вернитесь <a href='javascript:history.back(1)'><B>назад</B></a>. Вы указали неверные даные!";
-  }
-
-
-  else
-
-  {
-     $msg="
-
-  	<p>E-mail: $email</p>
-  	";
-
-  	//Отправляем письмо админу 
-  	mail("$adminemail", "$date $time Сообщение от $email");
-  	
-
-  // Выводим сообщение пользователю
-
-  print "<script language='Javascript'><!--function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); //--></script>
-
-  $msg 
-
-  <p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";
-  exit;
+<?php
+if( isset($_POST['email']) ) {
+	$email = trim($_POST['email']);
+	 if(empty($email)) {
+	 	echo 'Вы не заполнили поле!';
+	 }
+}else {
+	$mailto = 'kamida-agro@ukr.net';
+    $subject = 'Подписка с сайта';
+    $message .= 'E-mail подписчика: <a href="mailto:' . $email . '">' .$email . '</a><br />';
+    $headers = 'Content-type: text/html; charset=utf-8';
+    $mail = mail($mailto, $subject, $message, $headers);
+     if(TRUE === $mail) echo 'Ваше сообщение успешно отправлено!';
+     else echo 'Произошла ошибка при отправке сообщения.';
+ }
 }
-
 ?>
