b27610e655e25baf8eb47336b9ff1ca3d8ae0bec
Tepesha <leozaviriukha@ukr.net>
2018-02-13 19:56:08 +0200

    miami_js
    add js, slider

---
+++ miami/js/control.js
@@ -0,0 +1,61 @@
+$(function(){
+
+	'use strict';
+
+// Плавный переход по якроям
+
+	$('a[href^="#"]').click(function(){
+		let target = $(this).attr('href');
+		$('html, body').animate(
+		{
+			scrollTop:$(target).offset().top 
+		},
+		800
+		);
+	});
+
+
+// Фиксация меню при скролле
+
+	var offset = $('#bottom_header').offset();
+
+	$(window).scroll( function(){
+
+		if( $(window).scrollTop() > offset.top ) {
+			$('#bottom_header').addClass('fixedNav');
+		} else {
+			$('#bottom_header').removeClass('fixedNav');
+		}
+
+	} );
+
+
+// Плагин Slick
+
+	if( window.innerWidth > 1024 ) {
+		$('#slide').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+		});
+	} else {
+		$('#slide').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+		});
+	}
+
+// Добавление галлереи при клике на кнопку view
+
+	$('#view_all_gallery').click(function(){
+		let content = '<div class="gllery_line main_flex__nowrap flex__jcontent_between"> <div class="box_img_gllery ig_1"><img src="img/1ig.jpg" alt=""></div><div class="box_img_gllery ig_2"><img src="img/2ig.jpg" alt=""></div></div><div class="name_gallery">Marina Palms / <span class="blue">North Miami Beach, FL 33162</span></div>';
+		$('#gallery_box').append(content); 
+		$(this).remove();
+	});
+
+
+
+});
