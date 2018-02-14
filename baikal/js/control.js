80469151dc1e2825989f968f9794787ab88347a0
Tepesha <leozaviriukha@ukr.net>
2018-02-15 01:47:03 +0200

    baikal_js
    js

--- baikal/js/control.js
+++ baikal/js/control.js
@@ -1,8 +1,12 @@
-${function({
+$(function(){
 
 	"use strict";
 
-	
+	$('.title_box').click(function(){   //при нажатии активирвоать функции
+
+		$('.title_box').toggleClass('open'); //добавить класс
+		$('.title_box').next('div.list_link').toggleClass('open'); //добавить класс
+	});
 
 
-})};
+});
