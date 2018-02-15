3b0af07d6579be98c1d72eb3902ec135d4755355
Tepesha <leozaviriukha@ukr.net>
2018-02-15 20:05:17 +0200

    bikal_magazine_layout
    prise_jq,footer

--- baikal/js/control.js
+++ baikal/js/control.js
@@ -4,9 +4,26 @@ $(function(){
 
 	$('.title_box').click(function(){   //при нажатии активирвоать функции
 
-		$('.title_box').toggleClass('open'); //добавить класс
-		$('.title_box').next('div.list_link').toggleClass('open'); //добавить класс
+		$('this').toggleClass('open'); //добавить класс
+		$('this').next('div.list_link').toggleClass('open'); //добавить класс
 	});
+
+
+	$('#slider_price').slider({
+		max: 1000,
+		min: 0,
+		range: true,
+		values: [190,728],
+		slide: function( event, ui ) {
+			$('input[name="minPrice"]').val( '$' + ui.values[0] );
+			$('input[name="maxPrice"]').val(  '$' + ui.values[1] );
+		}
+	});
+
+	$('input[name="minPrice"]').val( '$' +
+	$('#slider_price').slider('values',0) );
+	$('input[name="maxPrice"]').val( '$' +
+	$('#slider_price').slider('values',1) );
 
 
 });
