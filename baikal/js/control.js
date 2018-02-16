e846314dceb5937a7db1003101f47d04736eb196
Tepesha <leozaviriukha@ukr.net>
2018-02-16 15:59:41 +0200

    baikal_magazine
    order_pannel,

--- baikal/js/control.js
+++ baikal/js/control.js
@@ -4,8 +4,8 @@ $(function(){
 
 	$('.title_box').click(function(){   //при нажатии активирвоать функции
 
-		$('this').toggleClass('open'); //добавить класс
-		$('this').next('div.list_link').toggleClass('open'); //добавить класс
+		$(this).toggleClass('open'); //добавить класс
+		$(this).next('div.list_link').toggleClass('open'); //добавить класс
 	});
 
 
@@ -24,6 +24,29 @@ $(function(){
 	$('#slider_price').slider('values',0) );
 	$('input[name="maxPrice"]').val( '$' +
 	$('#slider_price').slider('values',1) );
+	$('#cart, .title_cart').click(function(){
+		$('#cart_box').toggleClass('open');
+	});
 
+	$('#sign').click(function(){
+		$('#black_fill').toggleClass('open');
+		$('#modal').toggleClass('open');
+	});
+
+	$('#close_modal, #black_fill').click(function(){
+		$('#black_fill').toggleClass('open');
+		$('#modal').toggleClass('open');
+	});
+
+	$('#search_button').click(function(){
+		$('#search_pannel').toggleClass('open');
+	});
+
+	$('#load_more').click(function(event){
+		event.preventDefault();
+		$('.list_product').append('<a class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center">	<div class="tag box">new</div><div class="img_product">	<img src="img/01.png" alt=""></div><h2>Our Legacy Splash Jacquard Knit</h2>	<p>Black Grey Plants</p><p class="price">$290</p></a><a class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center">	<div class="tag box">new</div><div class="img_product">	<img src="img/01.png" alt=""></div><h2>Our Legacy Splash Jacquard Knit</h2>	<p>Black Grey Plants</p><p class="price">$290</p></a><a class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center">	<div class="tag box">new</div><div class="img_product">	<img src="img/01.png" alt=""></div><h2>Our Legacy Splash Jacquard Knit</h2>	<p>Black Grey Plants</p><p class="price">$290</p></a>');
+	});
+
+	
 
 });
