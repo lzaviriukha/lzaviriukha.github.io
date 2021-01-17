2da4478c5cf1c05292545bbaed1ca6448aecdeff
leozaviriukha <leozaviriukha@ukr.net>
2021-01-17 23:53:38 +0200

    add projects

---
+++ React-Game-of-Thrones-DB/build/precache-manifest.517909db7fd0a1db855c1eaf8354314a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "069f347df034a30d04d4",
+    "url": "/static/css/main.d20d3afd.chunk.css"
+  },
+  {
+    "revision": "069f347df034a30d04d4",
+    "url": "/static/js/main.069f347d.chunk.js"
+  },
+  {
+    "revision": "b79fb7757db8cae2c512",
+    "url": "/static/css/1.1caf5b0b.chunk.css"
+  },
+  {
+    "revision": "b79fb7757db8cae2c512",
+    "url": "/static/js/1.b79fb775.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8edd3d9334df9ad62e19d97aeefc6e7c",
+    "url": "/static/media/error.8edd3d93.jpg"
+  },
+  {
+    "revision": "587d3da7679b2346b77c6e5b4ba63404",
+    "url": "/index.html"
+  }
+];
