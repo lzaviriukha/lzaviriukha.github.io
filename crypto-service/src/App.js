110aa3b73498069ec811f077c9b4a8d48dde6689
Leo Zaviriukha <leozaviriukha@ukr.net>
2022-05-24 23:19:53 +0300

    add component and basic layout

--- crypto-service/src/App.js
+++ crypto-service/src/App.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./Components";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Cryptoverse</h1>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/exchanges" element={<Exchanges />} />
+                <Route path="/cryptocurrencies" element={<Cryptocurrencies />}/>
+                  <Route path="/crypto/:cointId" element={<CryptoDetails />} />
+             
+                <Route path="/news" element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
+        <div className="footer">
+            <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                Cryptoverse <br/>
+                All rights reserved
+            </Typography.Title>
+            <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+            </Space>
+        </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
